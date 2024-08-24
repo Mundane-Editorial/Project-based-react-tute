eb3e52eb928f1b079f4670be605b839ffcd69d1d
Mundane-Editorial <uday2k17@rediffmail.com>
2024-08-24 14:21:58 +0530

    added logic to textutils

--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -11,17 +11,37 @@ export default function TextForm(props) {
         console.log("on change");
         setText(event.target.value);
     }
+    const handleLowClick = () =>{
+        // console.log("upper case button clicked" + text);
+        let newtext = text.toLowerCase();
+        setText(newtext);
+    }
+    const handleLowChange = (event) =>{
+        console.log("on change");
+        setText(event.target.value);
+    }
 
-    const [text, setText] = useState('Enter Text here');
+    const [text, setText] = useState('Dummy text, can be remmoved. Just for reference');
     // setText("kjngdkgjjdsngds");
     return (
+        <>
             <div>
                 <h1>{props.heading}</h1>
                 <div className="mb-3">
                     {/* <label htmlFor="myBox" className='form-label'>Example text are</label> */}
                     <textarea className='form-control' value={text} id='myBox' rows="8" onChange={handleOnChange}></textarea>
                 </div>
-                    <button className='btn btn-primary' onClick={handleUpClick}>Convert to UpperCase</button>
+                    <button className='btn btn-primary mx-2' onClick={handleUpClick}>Convert to UpperCase</button>
+                    <button className='btn btn-primary mx-2' onClick={handleLowClick}>Convert to LowerCase</button>
             </div>
+            <div className="container my-3">
+                <h2>Your text summary</h2>        
+                <p>{text.length} characters, {text.split(" ").length} words.</p>
+                <p>{0.008 * text.split(" ").length} time will be required to read {text.split(" ").length} words.</p>
+                <hr />
+                <h2>preview text</h2>
+                <p>{text}</p>
+            </div>
+        </>
     )
 }
